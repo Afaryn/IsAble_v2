be0d5860335d54efa12a50312e9c7d98ec41ec11
afaryn <audy.fitri10@gmail.com>
2023-12-20 06:06:45 +0700

    update LearningDetail

---
+++ app/src/main/java/com/example/isable_capstone/ui/learningDetail/SubLearning.kt
@@ -0,0 +1,12 @@
+package com.example.isable_capstone.ui.learningDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.isable_capstone.R
+
+class SubLearning : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub_learning)
+    }
+}
